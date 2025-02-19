57a5f0f195bf2ad214b6125fb8d58fae1cb359d4
Etoile <etoile@example.com>
2025-02-19 15:20:44 +0900

    c

--- 00. 1st project/src/main/java/db/DBManager.java
+++ 00. 1st project/src/main/java/db/DBManager.java
@@ -29,8 +29,8 @@ public class DBManager {
 	
 	//2. 데이터베이스 연결
 	public void DBConnect() {
-		String url = "jdbc:mysql://localhost:3306/god";
-		String user = "root";
+		String url = "jdbc:mysql://192.168.0.60:3306/god";
+		String user = "wb";
 		String pw = "ezen";
 		try {
 			conn = DriverManager.getConnection(url, user, pw);
